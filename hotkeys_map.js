4855429825a82024c1852e9215185ab273f84ca2
yyk <yyk@mail.ru>
2018-01-28 00:53:16 +0300

    a demo of one hotkey described with AOC

---
+++ hotkeys_map.js
@@ -0,0 +1,12 @@
+function get_hotkeys_map(){
+  return {
+    by_id:{'k': {
+                ctrl:{
+                      description:'Open svg canvas',
+                      action:'Run, http://second.sedecilliard.com'
+                      }
+               },
+          },
+    all_ids:['k']
+  }
+}
