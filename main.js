f907d08e5e91c5ae836ee072602a0ad832ad077e
yyk <yyk@mail.ru>
2018-01-28 00:46:38 +0300

    some valuable code added for main

--- main.js
+++ main.js
@@ -1,7 +1,31 @@
 console.log('main process in kernel starts')
 var fs = require('fs');
-
-fs.readFile('my-file.txt', 'utf8', function(err, data) {
+const _ = require('underscore');
+const utils = require('./utils')
+fs.readFile('hotkey_map.ahk', 'utf8', function(err, data) {
     if (err) throw err;
-    console.log(data);
+    var items, hotkey_map;
+    items = data.split("Return");
+    hotkey_map = {}
+    items.forEach(function(item){
+      let lines, hotkey_code, kwd_btn;
+      lines = item.split("\n").filter((x)=>{return x.length >1 && !/^#/.test(x)})
+      if (lines.length) {
+          hotkey_code = lines[0].split('::')[0]
+          kwd_btn = hotkey_code.replace('!', '') .replace('^', '');
+          if (hotkey_map[kwd_btn] == undefined) {
+           hotkey_map[kwd_btn] = {};
+           var prefix = hotkey_code.replace(kwd_btn, '');
+           console.log(prefix);
+           if (prefix == '^'){
+             hotkey_map[kwd_btn].ctrl = {action:'action for ctrl ' + kwd_btn};
+           }
+
+           if (prefix == '^!'){
+             hotkey_map[kwd_btn].ctrlalt = {action:'action for ctrlalt ' + kwd_btn};
+           }
+         }
+      }
+    })
+    console.log(hotkey_map);
 });
